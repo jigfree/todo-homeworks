883de71c93ee1001d0727a77812530cc490721c5
jigfree <jigfree@gmail.com>
2019-02-07 10:22:38 +0900

    todo-app 프로젝트 생성

---
+++ 장인규/todo-app/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <h2>jigfree's Todo App</h2>
+    <todoInput>input</todoInput>
+    <todoList>list</todoList>
+    <button>RemoveAll</button>
+  </div>
+</template>
+
+<script>
+import todoInput from './components/TodoInput.vue'
+
+var TodoList = {
+   props:['propsdata'],
+  template:`<div>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>`,
+}
+
+export default {
+  data(){
+    return{
+      newTodoItem:'',
+    }
+  },
+  components: {
+    'todoInput': todoInput,
+    'todoList': TodoList,
+  }
+}
+</script>
+
+<style>
+ body {
+    text-align: center;
+    background-color: #F6F6F8;
+  }
+
+  input {
+    border-style: none;
+    width: 200px;
+  }
+  
+  button {
+    border-style: none;
+  }
+  
+ 
+</style>
---
+++ 장인규/todo-app/src/components/TodoInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="inputBox">
+    <input type="text" v-model="newTodoItem" placeholder="할일 입력하기" v-on:keyup.enter="addTodo">
+    <button class="addBtn" v-on:click="addTodo">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            newTodoItem:'',
+        }
+    }
+};
+</script>
+
+<style>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  text-align: left;
+  padding-left: 10px;
+  background: white;
+  height: 40px;
+  line-height: 40px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addBtn {
+  float: right;
+  height: 40px;
+  background:black;
+  display: inline-block;
+  width: 3rem;
+  font-size: 1.4rem;
+  border-radius: 0 5px 5px 0;
+  color: aliceblue;
+}
+</style>
